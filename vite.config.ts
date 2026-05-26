482323b401f1d52d6d3df3c68cc5d0f766ade3f2
Necron <necron3299@gmail.com>
2026-05-26 13:15:01 +0200

    clean up project structure, styles and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
